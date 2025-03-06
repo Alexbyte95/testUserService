bb44239965eb8ddddb52ea89f9fe184b6774cceb
Alexis Bautista <svedra95@gmail.com>
2025-03-05 22:22:09 -0600

    feat(dto): add user DTO for user creation

---
+++ src/dto/user.dto.ts
@@ -0,0 +1,12 @@
+export class CreateUserRequest {
+  name: string;
+  email: string;
+  age: number;
+}
+
+export class GetUserRequest {
+  id: string;
+  name: string;
+  email: string;
+  age: number;
+}
